c5ba42d9ccfcb4033241da0db92893feb1e32c5d
Vitaliy Efremov <freshman4000@gmail.com>
2020-12-18 00:30:07 +0300

    SFGPC-01 Created domain model of classes for app

---
+++ src/main/java/com/gurucourse/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.gurucourse.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/gurucourse/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.gurucourse.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
